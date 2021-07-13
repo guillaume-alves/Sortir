bd57e2402bc16cb092ff611cb017593d6d171112
Guillaume <guillaume.alves@netcourrier.com>
2021-07-13 16:04:42 +0200

    SortiesController creation

---
+++ src/Controller/SortiesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SortieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortiesController extends AbstractController
+{
+    /**
+     * @Route("/sorties", name="sorties")
+     */
+    public function index(SortieRepository $sortieRepository): Response
+    {
+        $sorties = $sortieRepository->findAll();
+        return $this->render('sorties/index.html.twig', [
+            'sorties' => $sorties
+        ]);
+    }
+}
