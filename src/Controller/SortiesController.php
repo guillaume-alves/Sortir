61c8b9cdb5c2cb7faa260a0048818a1e3bcf7478
Guillaume <guillaume.alves@netcourrier.com>
2021-07-16 09:40:24 +0200

    update

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Repository\CampusRepository;
+use App\Repository\EtatRepository;
+use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,6 +23,44 @@ class SortiesController extends AbstractController
         $sorties = $sortieRepository->findAll();
         return $this->render('sorties/index.html.twig', [
             'sorties' => $sorties
+        ]);
+    }
+
+    /**
+     * @Route("/creer", name="app_creer")
+     */
+    public function creer(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        ParticipantRepository $participantRepository,
+        EtatRepository $etatRepository,
+        CampusRepository $campusRepository
+    ) :  Response
+    {
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+
+        $sortie->setOrganisateur($participantRepository->findOneBy(['id' => 1]));
+        $sortie->setEtat($etatRepository->findOneBy(['id' => 1]));
+
+        $sortieForm->handleRequest($request);
+
+        if($sortieForm->isSubmitted() && $sortieForm->isValid())
+        {
+            $dataCampusNom = $sortieForm->getData()->getCampus()->getNom();
+            $campus = $campusRepository->findOneBy(['nom' => $dataCampusNom]);
+            $sortie->setCampus($campus);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            $this->addFlash('sucess', 'Une nouvelle sortie a été crée');
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('sorties/creer.html.twig', [
+            'sortieForm' => $sortieForm->createView(),
         ]);
     }
 
